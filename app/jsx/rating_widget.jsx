67ae75cf441ccd469d70399b84b49cb494f2c795
Ivan Calderon <ivan.calderon@crowdint.com>
2015-01-08 09:58:45 -0600

    Implement highlight state

--- app/jsx/rating_widget.jsx
+++ app/jsx/rating_widget.jsx
@@ -1,45 +1,90 @@
 /** @jsx React.DOM */
 'use strict';
 
-var RatingStar = React.createClass({
-
+var Star = React.createClass({
   render: function() {
     return (
       <li className="rating-star">
-        <span />
-        <input type="radio" name="rating" id={this.props.id}/>
-        <label htmlFor={this.props.id} onClick={this.props.onClick}/>
+        <a href=""
+        className={this.props.className}
+        onClick={this.props.onClick}
+        onMouseEnter={this.props.onMouseEnter}
+        onMouseLeave={this.props.onMouseLeave} />
       </li>
+      )
+  }
+});
+
+var RatingStars = React.createClass({
+  getInitialState: function() {
+    return {rating: 0};
+  },
+
+  handleStarClick: function(rating, e) {
+    e.preventDefault();
+    this.setState({rating: 0});
+    this.props.rating(rating);
+  },
+
+  setStarState: function(value) {
+    if (this.state.rating > 0) {
+      return this.state.rating >= value ? 'hovered' : 'inactive';
+    } else {
+      return this.props.selectedRating >= value ? 'active' : 'inactive';
+    }
+  },
+
+  handleOnMouseEnter: function(value) {
+    this.setState({rating: value});
+  },
+
+  handleOnMouseLeave: function() {
+    this.setState({rating: 0});
+  },
+
+  render: function() {
+    var stars = [1, 2, 3, 4, 5];
+    var self = this;
+    return (
+      <div className="rating-stars">
+        <ul>
+          {
+            stars.map(function(value) {
+              return (
+                  <Star className={self.setStarState.bind(self, value)()}
+                        onClick={self.handleStarClick.bind(self, value)}
+                        onMouseEnter={self.handleOnMouseEnter.bind(self, value)}
+                        onMouseLeave={self.handleOnMouseLeave} />
+                )
+            })
+          }
+        </ul>
+        <div className="rating-stars-footer">
+          Selected Rate <strong>{this.props.selectedRating}</strong>
+        </div>
+      </div>
       );
   }
 });
 
 var RatingBox = React.createClass({
   getInitialState: function() {
-    return {selectedRating: 0};
+    return {rating: 0};
   },
 
   setRating: function(rating) {
-    this.setState({selectedRating: rating});
+    this.setState({rating: rating});
   },
 
   render: function() {
     return (
       <div className="rating-box">
         <h1>Please Rate!</h1>
-        <ul>
-          <RatingStar onClick={this.setRating.bind(this, 1)} id="star-1"/>
-          <RatingStar onClick={this.setRating.bind(this, 2)} id="star-2"/>
-          <RatingStar onClick={this.setRating.bind(this, 3)} id="star-3"/>
-          <RatingStar onClick={this.setRating.bind(this, 4)} id="star-4"/>
-          <RatingStar onClick={this.setRating.bind(this, 5)} id="star-5"/>
-        </ul>
-        <div className="rating-box-footer">
-          Selected Rate <strong>{this.state.selectedRating}</strong>
-        </div>
+        <RatingStars rating={this.setRating} selectedRating={this.state.rating} />
       </div>
-    );
+      )
   }
+
 });
 
 React.renderComponent(
