735369bd5948199b808d91c22a5822e23eeb9096
Ivan Calderon <ivan.calderon@crowdint.com>
2015-01-07 18:10:44 -0600

    Use images for rating stars

--- Gruntfile.js
+++ Gruntfile.js
@@ -190,7 +190,7 @@ module.exports = function (grunt) {
         // Performs rewrites based on rev and the useminPrepare configuration
         usemin: {
             options: {
-                assetsDirs: ['<%= yeoman.dist %>']
+                assetsDirs: ['<%= yeoman.dist %>', '<%= yeoman.dist %>/images']
             },
             html: ['<%= yeoman.dist %>/{,*/}*.html'],
             css: ['<%= yeoman.dist %>/styles/{,*/}*.css']
@@ -267,7 +267,8 @@ module.exports = function (grunt) {
         compass: {
           options: {
             sassDir: '<%= yeoman.app %>/styles',
-            cssDir: '.tmp/styles'
+            cssDir: '.tmp/styles',
+            imagesDir: '<%= yeoman.app %>/images'
           },
           dev: {
             environment: 'development'
